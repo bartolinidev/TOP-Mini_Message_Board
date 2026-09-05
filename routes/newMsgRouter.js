7e22e4a011ecdb5abc6494630402d3dd495370f9
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-09-06 01:13:14 +0200

    Add POST handling

--- routes/newMsgRouter.js
+++ routes/newMsgRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const newMsgRouter = Router();
+const { addMessage } = require('../controllers/newMsgController');
 
 // GET /new
 newMsgRouter.get('/', (req, res) => res.send('New message creator'));
 
 // POST /new
-newMsgRouter.post('/', (req, res) => res.send('New message sent!'));
+newMsgRouter.post('/', addMessage);
 
 module.exports = newMsgRouter;
