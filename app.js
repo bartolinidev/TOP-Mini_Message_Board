e73fccb63926d7cd864b8c1cdc43c05276790d75
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-09-05 21:44:37 +0200

    Set indexRouter, and port

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const indexRouter = require('./routes/indexRouter');
 
+app.use('/', indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Listening on port ${PORT}!`);
+});
